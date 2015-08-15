f0d29aa85a409e606815e36659b95185452476d3
jpoorni <jpoorni@yahoo.com>
2015-08-15 12:59:57 -0700

    Completed Ex5

--- CSExercises/Ex01-06/Ex05.cs
+++ CSExercises/Ex01-06/Ex05.cs
@@ -10,7 +10,13 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            double dblnumber = 0;
+            double dblsquareroot = 0;
+
+            System.Console.Write("Enter a decimal number : ");
+            dblnumber = Convert.ToDouble(System.Console.ReadLine());
+            dblsquareroot = Math.Sqrt(dblnumber);
+            System.Console.WriteLine("The square of {0} = {1:#0.000} ", dblnumber, dblsquareroot);
         }
     }
 }
