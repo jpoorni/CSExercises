adc795f68b63c1855cb6c6fbb8b358809abb8daf
jpoorni <jpoorni@yahoo.com>
2015-08-15 14:57:02 -0700

    Completed Ex11

--- CSExercises/Ex07-15/Ex11.cs
+++ CSExercises/Ex07-15/Ex11.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Calculate Taxi Fare
+//Minimum fixed charge : $2.40
+//In addition the fare would be computed at 40 cents per kilometer
+//print the fare without any number formating
+
 namespace CSExercises
 {
     public class Ex11
     {
         public static void Main(string[] args)
         {
-            
+            double dblFixedCharge = 2.40, dblPerKilometerCharge = 0.4; //40 cents per kilometer
+            double dblDistanceTravelled = 0, dblTaxiFare = 0;
+
+            System.Console.Write("Enter the distance travelled :");
+            dblDistanceTravelled = Convert.ToDouble(System.Console.ReadLine());
+
+            dblTaxiFare = dblFixedCharge + (dblDistanceTravelled * dblPerKilometerCharge);
+            System.Console.WriteLine("Taxi Fare = {0}",dblTaxiFare);
         }
     }
 }
