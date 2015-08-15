51d7aacdeedbdfcaa86f8a516ea36bb2b5272492
jpoorni <jpoorni@yahoo.com>
2015-08-15 14:06:42 -0700

    Completed Ex8

--- CSExercises/Ex07-15/Ex08.cs
+++ CSExercises/Ex07-15/Ex08.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//converting centigrade to farenheit
+
 namespace CSExercises
 {
     public class Ex08
     {
         public static void Main(string[] args)
         {
-            
+            double dblCentigrade = 0 , dblFarenheit;
+
+            System.Console.Write("Enter temperature in Centigrade scale :");
+            dblCentigrade = Convert.ToDouble(System.Console.ReadLine());
+            dblFarenheit = (1.8 * dblCentigrade) + 32;
+            System.Console.WriteLine("\n{0} Centigrade  = {1} Farenheit " , dblCentigrade, dblFarenheit);
         }
     }
 }
