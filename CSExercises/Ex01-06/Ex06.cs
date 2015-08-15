4deecac055e06c274c338292e2a0ace7b8e41b09
jpoorni <jpoorni@yahoo.com>
2015-08-15 13:33:29 -0700

    Completed Ex6

--- CSExercises/Ex01-06/Ex06.cs
+++ CSExercises/Ex01-06/Ex06.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//input integer and find its square root
+
 namespace CSExercises
 {
     public class Ex06
     {
         public static void Main(string[] args)
         {
-            
+            int intNumber = 0;
+            double dblSqrt = 0;
+
+            System.Console.Write("Enter a integer number :");
+            intNumber = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine(intNumber);
+            //sqrt funtion can take arguments only as double, here the input integer is converted to double
+            dblSqrt = Math.Sqrt(Convert.ToDouble(intNumber)); 
+
+            System.Console.WriteLine(dblSqrt);
         }
     }
 }
