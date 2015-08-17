fb88c14dfcd6a1e0618660716878c28abf00e96b
jpoorni <jpoorni@yahoo.com>
2015-08-17 11:23:44 -0700

    Completed Ex16

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,10 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Good morning {Mr/Ms} {name}
+
 namespace CSExercises
 {
     public class Ex16
     {
         public static void Main(string[] args)
         {
-            
+            string strName = "";
+            string strGender = "";
+
+            System.Console.Write("Enter your name :");
+            strName = System.Console.ReadLine();
+            System.Console.Write("Enter gender :(F = Female , M = Male)");
+            strGender = System.Console.ReadLine();
+
+           if (strGender == "M")
+           {
+               System.Console.WriteLine("Good Morning Mr. {0}",strName);
+           }
+           else
+           {
+               System.Console.WriteLine("Good Morning Ms. {0}", strName);
+           }
         }
     }
 }
